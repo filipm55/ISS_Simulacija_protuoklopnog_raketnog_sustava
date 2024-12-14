a1bb1caa4ec7341bedba3f313b798927193d3dc5
Marko <marko.grguric2@fer.unizg.hr>
2024-12-14 15:32:52 +0100

    Eksplozija
    Dodana je primitivna eksplozija, vjerojatno zelimo ralisiticniju eksploziju s nekim silama prema okolosu nakon sto raketa eksplordira

--- Assets/Scripts/Missile.cs
+++ Assets/Scripts/Missile.cs
@@ -5,20 +5,26 @@ public class Missile : MonoBehaviour
     public float speed = 0.5f;
     public float lifeTime = 5f;
 
+    [SerializeField] GameObject explosionPrefab;
+
     void Start()
     {
-        Destroy(gameObject, lifeTime);  // Destroy after time
+        Destroy(gameObject, lifeTime);  // Objekt se brise nakon nekog vremena
     }
 
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);  // Move forward
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);  // Kretanje naprijed
     }
 
     void OnCollisionEnter(Collision collision)
     {
         
         Debug.Log("Missile hit: " + collision.gameObject.name);
+        // eksplozija na mjestu sudara
+        GameObject explosion = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        Destroy (explosion, 2f);
+
         if (collision.gameObject.CompareTag("enemy"))
         {
             Destroy(collision.gameObject);
